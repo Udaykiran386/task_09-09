3b70c68fa5dbdb2da6df9431304f048a535e83eb
Udaykiran386 <udaykiranraga@gmail.com>
2026-09-10 16:55:39 +0530

    prompt is also add

--- config.py
+++ config.py
@@ -17,4 +17,4 @@ DB_CONFIG = {
 DATABASE_URL = (
     f"postgresql+psycopg2://{DB_CONFIG['user']}:{DB_CONFIG['password']}"
     f"@{DB_CONFIG['host']}:{DB_CONFIG['port']}/{DB_CONFIG['database']}"
-)
+) 
